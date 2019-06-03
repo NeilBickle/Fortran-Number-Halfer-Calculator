d0cb9b3032ea3ff39e9585fc564ebdc9ee1d5bb4
Neil Bickle (AMBCS) (AssocIScT) (TMIET) (ICTTech) <43986316+NeilBickle@users.noreply.github.com>
2019-06-03 13:24:19 +0100

    Ver 1

---
+++ main.f95
@@ -0,0 +1,9 @@
+program NumberHalfer
+    real :: c, HalfedAnswer
+    print *, "Welcome To The Number Halfer"
+    print *, "Type A Number: "
+    read *, c
+    HalfedAnswer = c / 2
+    print *, c, " / ", 2, " = ", HalfedAnswer
+    read *
+end program NumberHalfer
